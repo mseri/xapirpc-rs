fbd62479575f5e93e3ecd35cebaa6f43b987f26c
Marcello Seri <marcello.seri@citrix.com>
2017-11-01 11:37:54 +0000

    Small format update
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use clap::{Arg, App};
 use xmlrpc::{Request, Value};
 use reqwest::Client;
 
+
 fn to_value(value: &str) -> Value {
     bool::from_str(value)
         .map(|b| Value::Bool(b))
@@ -22,6 +23,9 @@ fn to_value(value: &str) -> Value {
                     )))
 }
 
+
+// The two functions below should be changed to return a Result
+// instead of panicking
 
 fn get_value(res: &Value) -> &Value {
     if let Value::Struct(ref response) = *res {
@@ -42,7 +46,6 @@ fn extract_session(res: Value) -> String {
 
 
 fn main() {
-
     let matches = App::new("Dummy xapi xmlrpc CLI client")
         .version("0.1")
         .author("Marcello S. <marcello.seri@citrix.com>")
@@ -51,77 +54,84 @@ fn main() {
              .long("host")
              .value_name("HOST")
              .env("XAPI_HOST")
-             .help("XenServer host")
+             .help("XenServer host. Can be passed with the HOST env variable.")
              .takes_value(true))
         .arg(Arg::with_name("user")
              .short("u")
              .long("user")
              .value_name("USER")
              .env("XAPI_USER")
-             .help("XenServer host user name")
+             .help("XenServer host user name. Can be passed with the \
+                   XAPI_USER env variable.")
              .takes_value(true))
         .arg(Arg::with_name("pass")
              .short("p")
              .long("pass")
              .value_name("PASSWORD")
              .env("XAPI_PASSWORD")
-             .help("XenServer host user password")
+             .help("XenServer host user password. Can be passed with the \
+                   XAPI_PASSWORD env variable.")
              .takes_value(true))
         .arg(Arg::with_name("class")
              .value_name("CLASS")
-             .help("Case sensitive value for the xapi class")
+             .help("Case sensitive value for the xapi class.")
              .required(true)
              .index(1))
         .arg(Arg::with_name("method")
              .value_name("METHOD")
-             .help("Case sensitive value for the xapi method")
+             .help("Case sensitive value for the xapi method.")
              .required(true)
              .index(2))
         .arg(Arg::with_name("args")
              .value_name("ARGS")
-             .help("Ordered list of arguments for the call (if any)")
+             .help("Ordered list of arguments for the call (if any). \
+                   Do not pass a session.")
              .multiple(true))
         .get_matches();
 
     let host = matches.value_of("host").unwrap_or("http://127.0.0.1");
-    let user = matches.value_of("user").unwrap_or("root");
-    let pass = matches.value_of("pass").unwrap_or("xenroot");
+    let user = matches.value_of("user").unwrap_or("guest");
+    let pass = matches.value_of("pass").unwrap_or("guest");
 
+    // These are compulsory parameters. unwrapping here is fine
     let class = matches.value_of("class").unwrap();
     let method = matches.value_of("method").unwrap();
 
-    let args =
-        if matches.is_present("args") {
-            matches.values_of("args").unwrap().collect()
-        } else {
-            Vec::new()
-        }
-    .into_iter()
+    let args = if matches.is_present("args") {
+        matches.values_of("args").unwrap().collect()
+    } else {
+        Vec::new()
+    }.into_iter()
         .map(|a| to_value(&a));
 
     let client = Client::new();
 
     // Let's panic all the way!!!
-    let hopefully_session = Request::new("session.login_with_password").arg(user).arg(pass)
+    let hopefully_session =
+        Request::new("session.login_with_password")
+        .arg(user).arg(pass)
         .call(&client, host)
-        .unwrap() // Response
+        .unwrap()  // Response
         .unwrap(); // Result
     let session = extract_session(hopefully_session);
     //println!("Session: \"{}\"", session);
 
     // Prepare the xmlrpc command
     let cmd = format!("{}.{}", class, method);
-    let mut req = Request::new(&cmd).arg(session.clone());
+    let mut req =
+        Request::new(&cmd)
+        .arg(session.clone());
     for arg in args {
         req = req.arg(arg);
     }
 
-    //let mut body = Vec::new();
-    //req.write_as_xml(&mut body).unwrap();
-    //println!("{}", std::str::from_utf8(&body).unwrap());
-
+    // Again, we unwrap the Response and then the Result
+    // don't care about the panics right now.
     let response = req.call(&client, host).unwrap().unwrap();
     println!("{:?}", get_value(&response));
 
-    let _ = Request::new("session.logout").arg(session).call(&client, host);
+    let _ =
+        Request::new("session.logout")
+        .arg(session)
+        .call(&client, host);
 }
