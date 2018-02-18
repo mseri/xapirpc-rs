18dee82d7765ac60af3e125a3758dca1c3ee639f
Marcello Seri <marcello.seri@citrix.com>
2018-02-18 12:33:47 +0000

    xapirpc: rustfmt code
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,16 @@ extern crate xmlrpc;
 use std::str::FromStr;
 
 use base64::encode;
-use clap::{Arg, App};
+use clap::{App, Arg};
 use preferences::{AppInfo, Preferences, PreferencesMap};
 use reqwest::Client;
 use serde_json::value as json;
 use xmlrpc::{Request, Value};
 
+const APP_INFO: AppInfo = AppInfo {
+    name: "xapirpc",
+    author: "xapirpc",
+};
 
 fn heuristic_to_value(value: &str) -> Value {
     if let Ok(b) = bool::from_str(value) {
@@ -36,17 +40,20 @@ fn heuristic_to_value(value: &str) -> Value {
 }
 
 // From xmlrpc-rs' utils.rs
-pub fn format_datetime(date_time: &iso8601::DateTime) -> String {
-    let iso8601::Time { hour, minute, second, .. } = date_time.time;
+fn format_datetime(date_time: &iso8601::DateTime) -> String {
+    let iso8601::Time {
+        hour,
+        minute,
+        second,
+        ..
+    } = date_time.time;
 
     match date_time.date {
-        iso8601::Date::YMD { year, month, day } => {
-            format!("{:04}{:02}{:02}T{:02}:{:02}:{:02}",
-                year, month, day,
-                hour, minute, second
-            )
-        }
-        _ => { unimplemented!() }
+        iso8601::Date::YMD { year, month, day } => format!(
+            "{:04}{:02}{:02}T{:02}:{:02}:{:02}",
+            year, month, day, hour, minute, second
+        ),
+        _ => unimplemented!(),
     }
 }
 
@@ -113,66 +120,84 @@ fn main() {
     let matches = App::new("Minimal xapi xmlrpc CLI client")
         .about("CLI interface to interrogate an instance of XenServer via xmlrpc")
         .version(crate_version!())
-        .arg(Arg::with_name("host")
-             .long("host")
-             .value_name("HOST")
-             .env("XAPI_HOST")
-             .help("XenServer host. Can be passed with the HOST env variable.")
-             .takes_value(true))
-        .arg(Arg::with_name("user")
-             .short("u")
-             .long("user")
-             .value_name("USER")
-             .env("XAPI_USER")
-             .help("XenServer host user name. Can be passed with the \
-                   XAPI_USER env variable.")
-             .takes_value(true))
-        .arg(Arg::with_name("pass")
-             .short("p")
-             .long("pass")
-             .value_name("PASSWORD")
-             .env("XAPI_PASSWORD")
-             .help("XenServer host user password. Can be passed with the \
-                   XAPI_PASSWORD env variable.")
-             .takes_value(true))
-        .arg(Arg::with_name("compact")
-             .long("compact")
-             .help("Output the result as non-prettified json.")
-            )
-        .arg(Arg::with_name("class")
-             .value_name("CLASS")
-             .help("Case sensitive value for the xapi class.")
-             .required(true)
-             .index(1))
-        .arg(Arg::with_name("method")
-             .value_name("METHOD")
-             .help("Case sensitive value for the xapi method.")
-             .required(true)
-             .index(2))
-        .arg(Arg::with_name("args")
-             .value_name("ARGS")
-             .help("Ordered list of arguments for the call (if any). \
-                   Do not pass a session.")
-             .multiple(true))
+        .arg(
+            Arg::with_name("host")
+                .long("host")
+                .value_name("HOST")
+                .env("XAPI_HOST")
+                .help("XenServer host. Can be passed with the HOST env variable.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("user")
+                .short("u")
+                .long("user")
+                .value_name("USER")
+                .env("XAPI_USER")
+                .help(
+                    "XenServer host user name. Can be passed with the \
+                     XAPI_USER env variable.",
+                )
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("pass")
+                .short("p")
+                .long("pass")
+                .value_name("PASSWORD")
+                .env("XAPI_PASSWORD")
+                .help(
+                    "XenServer host user password. Can be passed with the \
+                     XAPI_PASSWORD env variable.",
+                )
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("compact")
+                .long("compact")
+                .help("Output the result as non-prettified json."),
+        )
+        .arg(
+            Arg::with_name("class")
+                .value_name("CLASS")
+                .help("Case sensitive value for the xapi class.")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("method")
+                .value_name("METHOD")
+                .help("Case sensitive value for the xapi method.")
+                .required(true)
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("args")
+                .value_name("ARGS")
+                .help(
+                    "Ordered list of arguments for the call (if any). \
+                     Do not pass a session.",
+                )
+                .multiple(true),
+        )
         .get_matches();
+
+    let preferences = PreferencesMap::<String>::load(&APP_INFO, "config")
+        .unwrap_or(PreferencesMap::<String>::new());
 
     let host_default = "http://127.0.0.1".to_string();
     let user_default = "guest".to_string();
     let pass_default = "guest".to_string();
 
-    let preferences = PreferencesMap::<String>::load(&APP_INFO, "config").unwrap_or(
-        PreferencesMap::<String>::new()
-        //config.insert("user".into(), "guest".into());
-        //config.insert("pass".into(), "guest".into());
-        //config.inster("host".into(), "http://127.0.0.1".into());
-        );
-
-    let host = matches.value_of("host").unwrap_or(
-        preferences.get("host").unwrap_or(&host_default));
-    let user = matches.value_of("user").unwrap_or(
-        preferences.get("user").unwrap_or(&user_default));
-    let pass = matches.value_of("pass").unwrap_or(
-        preferences.get("pass").unwrap_or(&pass_default));
+    let host = matches
+        .value_of("host")
+        .unwrap_or(preferences.get("host").unwrap_or(&host_default));
+    let user = matches
+        .value_of("user")
+        .unwrap_or(preferences.get("user").unwrap_or(&user_default));
+    let pass = matches
+        .value_of("pass")
+        .unwrap_or(preferences.get("pass").unwrap_or(&pass_default));
 
     // These are compulsory parameters. unwrapping here is fine
     let class = matches.value_of("class").unwrap();
@@ -188,8 +213,7 @@ fn main() {
     let client = Client::new();
 
     // Let's panic all the way!!!
-    let hopefully_session =
-        Request::new("session.login_with_password")
+    let hopefully_session = Request::new("session.login_with_password")
         .arg(user).arg(pass)
         .call(&client, host)
         .unwrap()  // Response
@@ -199,9 +223,7 @@ fn main() {
 
     // Prepare the xmlrpc command
     let cmd = format!("{}.{}", class, method);
-    let mut req =
-        Request::new(&cmd)
-        .arg(session.clone());
+    let mut req = Request::new(&cmd).arg(session.clone());
     for arg in args {
         req = req.arg(arg);
     }
@@ -218,8 +240,7 @@ fn main() {
     };
     println!("{}", j.unwrap());
 
-    let _ =
-        Request::new("session.logout")
+    let _ = Request::new("session.logout")
         .arg(session)
         .call(&client, host);
 }
